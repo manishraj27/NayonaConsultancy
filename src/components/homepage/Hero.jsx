7b88509132639d3fd931f97720469c078c921360
manishraj27 <manish012raj@gmail.com>
2025-01-11 16:42:25 +0530

    add react-router-dom for routing and update NavBar menu items based on current location

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -3,45 +3,7 @@
 function Hero() {
   return (
     <div className="container mx-auto px-4 py-8 lg:py-64">
-      <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
-        <p className="text-center text-gray-600">This is the hero section of the website.</p>
-        <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
-        
+     
         <h1 className="text-4xl font-bold text-center text-gray-800">Hero Section</h1>
         <p className="text-center text-gray-600">This is the hero section of the website.</p>
         <p className="text-center text-gray-600">It is the first thing that users see when they visit the website.</p>
--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import HeaderButtons from "./HeaderButtons";
 import ArrowIcon from "../../assets/icons/ArrowIcon";
+import { useLocation } from 'react-router-dom';
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const location = useLocation(); // Get the current location
   const menuItems = [
-    { name: "Home", href: "/", disabled: true },
-    { name: "About Us", href: "/about" },
-    { name: "Services", href: "/services" },
-    { name: "Blog", href: "/blog" },
-    { name: "Contact", href: "/contact" },
+    { name: "Home", href: "/", disabled: location.pathname === "/" },
+    { name: "About Us", href: "/about", disabled: location.pathname === "/about" },
+    { name: "Services", href: "/services", disabled: location.pathname === "/services" },
+    { name: "Blog", href: "/blog", disabled: location.pathname === "/blog" },
+    { name: "Contact", href: "/contact", disabled: location.pathname === "/contact" },
   ];
 
   return (
