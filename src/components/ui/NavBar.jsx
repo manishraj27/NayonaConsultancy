016396c612050742c45086811fe2f8f8e8a41866
manishraj27 <manish012raj@gmail.com>
2025-01-11 15:51:52 +0530

    remove unused Nayona icon and refactor NavBar for improved layout and functionality

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -15,45 +15,34 @@ const NavBar = () => {
 
   return (
     <div className="relative">
-      <header className="flex flex-row items-center justify-between px-4 lg:px-16 py-4 lg:py-12">
+      <header className="flex flex-row items-center justify-between px-4 lg:px-16 py-8 lg:py-12">
       <HeaderButtons isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       </header>
 
       {/* Navigation Menu */}
-      <nav
-        className={`
-          fixed top-0 left-0 z-50 w-full h-full bg-[rgba(0,0,0,0.5)]
-          transition-transform duration-300 ease-out transform
-          lg:hidden
-
-          ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}
+      <nav className="w-1/4 fixed right-0 top-64 lg:top-0 select-none pointer-events-auto z-[3] px-2 lg:px-4">
+  <div className="rounded-3xl bg-gray-100 px-4 lg:px-6 py-4 lg:py-6 flex flex-col gap-2">
+    {menuItems.map((item) => (
+      <a
+        key={item.name}
+        href={item.href}
+        className={`flex group flex-row items-center gap-3 
+          text-xl lg:text-2xl font-semibold 
+          py-2 lg:py-3 rounded-2xl w-full px-4
+          hover:bg-white transition-colors duration-200 ease-out
+          ${item.disabled ? 'text-gray-400 pointer-events-none' : 'text-gray-800 pointer-events-auto'}
         `}
       >
-        <div className="rounded-3xl bg-red-500 px-6 lg:px-6 py-8 lg:py-12 flex flex-col gap-2">
-          {menuItems.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className={`
-                flex group flex-row items-center gap-4 
-                text-3xl lg:text-5xl font-semibold 
-                py-1 lg:py-4 rounded-2xl w-full px-6
-                hover:bg-white transition-colors duration-200 ease-out
-                ${
-                  item.disabled
-                    ? "text-gray-400 pointer-events-none"
-                    : "text-gray-800 pointer-events-auto"
-                }
-              `}
-            >
-              <span className="w-0 group-hover:w-7 transition-[width] duration-300 ease-out hidden lg:block">
-                <ArrowIcon />
-              </span>
-              <span>{item.name}</span>
-            </a>
-          ))}
-        </div>
-      </nav>
+        <span className="w-0 group-hover:w-5 transition-[width] duration-300 ease-out hidden lg:block">
+          <ArrowIcon />
+        </span>
+        <span>{item.name}</span>
+      </a>
+    ))}
+  </div>
+</nav>
+
+
     </div>
   );
 };
