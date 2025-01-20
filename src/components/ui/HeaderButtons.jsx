8a95668258dc67b72eb7a33dfa70d4d0981a8ea1
manishraj27 <manish012raj@gmail.com>
2025-01-20 19:22:49 +0530

    update HeaderButtons component to change text color and remove LogoWithBrandName

--- src/components/ui/HeaderButtons.jsx
+++ src/components/ui/HeaderButtons.jsx
@@ -7,7 +7,7 @@ const HeaderButtons = ({ isMenuOpen, setIsMenuOpen }) => {
   return (
     <div className="fixed flex flex-row items-center justify-between w-full px-4 lg:px-16 py-4 lg:py-8 z-[10]">
       {/* Logo Section */}
-      <div className="text-[#1d383e] text-2xl lg:text-3xl font-bold tracking-wider flex items-center">
+      <div className="text-[#081012] text-2xl lg:text-3xl font-bold tracking-wider flex items-center">
         <LogoIcon />
         {/* <LogoWithBrandName /> */}
       </div>
