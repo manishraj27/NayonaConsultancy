9f0323c797046595b8201b0b9fbd46664560fdcc
manishraj27 <manish012raj@gmail.com>
2025-02-17 12:49:19 +0530

    feat: refine layout and spacing in Contact and GlobeNayona components for enhanced visual appeal and responsiveness

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -6,13 +6,13 @@ function Contact() {
     <section
       id="contact"
       aria-label="contact"
-      className="overflow-hidden lg:px-12 px-4 w-full py-24 h-screen lg:h-screen dark-section bg-background-100 flex items-center justify-between"
+      className="overflow-hidden lg:px-12 px-4 w-full py-24 min-h-screen dark-section bg-background-100 flex items-center justify-between"
     >
       {/* Left Content - Heading and Text */}
       <div className="text-white w-3/4">
         <Heading title="Reach To Us" description="START CONVERSTION" />
         <div className="mt-4 w-2/3">
-          <span className="text-white text-heading-4 font-grotesk text-pretty block">
+          <span className="text-white text-heading-4 mx-44  w-3/4 font-grotesk text-pretty block">
             Start a conversation about new business opportunities and get a free consultation.
           </span>
         </div>
--- src/components/ui/GlobeNayona.jsx
+++ src/components/ui/GlobeNayona.jsx
@@ -3,12 +3,12 @@ import { Globe } from "../magicui/Globe";
 
 export function GlobeDemo() {
   return (
-    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg px-40 pb-40 pt-32 md:pb-64">
+    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg px-40 pb-40 pt-16 md:pb-72">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-7xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Our Reach is Global
       </span>
-      <Globe className="top-96" />
-      <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+      <Globe className="top-80" />
+      {/* <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" /> */}
     </div>
   );
 }
