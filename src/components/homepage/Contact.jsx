644b4eca7d877b5c05b0ebeacb52d427ab04724a
manishraj27 <manish012raj@gmail.com>
2025-02-17 12:28:55 +0530

    feat: enhance layout and styling in Contact and GlobeNayona components for improved visual hierarchy and responsiveness

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -1,4 +1,3 @@
-
 import Heading from "../ui/Heading";
 import { GlobeDemo } from "../ui/GlobeNayona";
 
@@ -7,18 +6,24 @@ function Contact() {
     <section
       id="contact"
       aria-label="contact"
-      className="overflow-hidden lg:px-12 px-4 w-full py-24 lg:24 h-screen dark-section bg-background-100"
+      className="overflow-hidden lg:px-12 px-4 w-full py-24 h-screen lg:h-screen dark-section bg-background-100 flex items-center justify-between"
     >
-      <div className="my-[25%] md:my-[9%] overflow-hidden text-white ">
+      {/* Left Content - Heading and Text */}
+      <div className="text-white w-3/4">
         <Heading title="Reach To Us" description="START CONVERSTION" />
-        <div className="mt-4 w-1/3 mx-64"> {/* Centers and limits width */}
+        <div className="mt-4 w-2/3">
           <span className="text-white text-heading-4 font-grotesk text-pretty block">
             Start a conversation about new business opportunities and get a free consultation.
           </span>
         </div>
       </div>
 
-      <GlobeDemo />
+      {/* Right Content - Globe */}
+      <div className="w-1/4 flex justify-end">
+        <GlobeDemo />
+      </div>
+
+
     </section>
   );
 }
