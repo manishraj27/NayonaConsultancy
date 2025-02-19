250eba9dc4645294d3643b07b9c194bedfe2fdb0
manishraj27 <manish012raj@gmail.com>
2025-02-19 15:10:11 +0530

    feat: adjust padding in FooterContent for better responsiveness

--- src/components/ui/FooterContent.jsx
+++ src/components/ui/FooterContent.jsx
@@ -1,6 +1,6 @@
 export default function FooterContent() {
   return (
-    <div className=" text-black py-10 px-12 h-full w-full flex flex-col justify-between">
+    <div className=" text-black py-10 lg:px-12 px-4 h-full w-full flex flex-col justify-between">
 
         {/* Yet to implement footer design */}
       <div className="flex justify-between">
