6bd3fb68c12ccfb470d2e4c8d29c879299492b9b
manishraj27 <manish012raj@gmail.com>
2025-02-20 15:09:52 +0530

    feat: update Footer component height to 100vh for improved layout

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -4,10 +4,10 @@ import FooterContent from "./FooterContent"
 function Footer() {
   return (
     <footer 
-      className='relative h-[580px] bg-light-200'
+      className='relative h-[100vh] bg-light-200'
       style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
     >
-      <div className='fixed bottom-0 h-[580px] w-full'>
+      <div className='fixed bottom-0 h-[100vh] w-full'>
         <FooterContent />
       </div>
     </footer>
