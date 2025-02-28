ed3e55712974e4703f9c12abd11396e104fdf35f
manishraj27 <manish012raj@gmail.com>
2025-03-01 00:33:52 +0530

    feat: add DotPattern component for decorative background in FooterContent; refactor layout for improved structure

--- src/components/ui/FooterContent.jsx
+++ src/components/ui/FooterContent.jsx
@@ -1,20 +1,38 @@
 
+import { cn } from "../../lib/utils";
+import DotPattern from "../magicui/DotPattern";
 import FooterBottom from "./FooterBottom";
 import FooterCredits from "./FooterCredits";
 import FooterUpper from "./FooterUpper";
 
 function FooterContent() {
   return (
-    <div className="h-full w-full flex flex-col justify-end pb-4 px-4 lg:px-12">
+    <>
+      
+      <div className="h-full w-full flex flex-col justify-end pb-4 px-4 lg:px-12">
+     
+        <FooterUpper />
 
-      <FooterUpper />
 
+        <div className="w-full flex flex-col lg:flex-row items-center justify-between lg:gap-24">
+          <FooterBottom />
+        </div>
+        <FooterCredits />
+       
+      <DotPattern
+        width={20}
+        height={20}
+        cx={1}
+        cy={1}
+        cr={1}
+        className={cn(
+          "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
+        )}
+      />
 
-      <div className="w-full flex flex-col lg:flex-row items-center justify-between lg:gap-24">
-        <FooterBottom />
       </div>
-      <FooterCredits />
-    </div>
+    </>
+
   );
 }
 
