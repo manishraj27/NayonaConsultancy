8b7761d73fb0afa55d883bf2af6b2b9a1620a0da
manishraj27 <manish012raj@gmail.com>
2025-02-28 23:24:52 +0530

    feat: update FooterContent layout and copyright section; adjust font size and separator styling

--- src/components/ui/FooterContent.jsx
+++ src/components/ui/FooterContent.jsx
@@ -10,19 +10,23 @@ function FooterContent() {
     <div className="h-full w-full flex flex-col justify-end pb-4 px-4 lg:px-12">
 
 
+
+      <div className="w-full my-3 px-4">
+        <div className="h-px bg-gray-300"></div>
+      </div>
+
       <div className="w-full flex flex-col lg:flex-row items-center justify-between lg:gap-24">
         <FooterBottom />
       </div>
-      
-      <div className="w-full my-4 px-4">
+      <div className="w-full my-3 px-4">
         <div className="h-px bg-gray-300"></div>
       </div>
 
       {/* "All Rights Reserved" */}
       <div className="w-full flex justify-between items-center px-4">
         {/* All Rights Reserved */}
-        <span className="flex items-center text-gray-700 font-grotesk text-body-5 lg:text-body-5">
-          <Copyright className="w-4 h-4 mr-2" /> {new Date().getFullYear()} All Rights Reserved
+        <span className="flex items-center text-gray-700 font-grotesk text-body-5 lg:text-body-4">
+          <Copyright className="w-3 h-3 mr-2" /> {new Date().getFullYear()} All Rights Reserved
         </span>
 
         {/* Website by Him */}
@@ -34,7 +38,7 @@ function FooterContent() {
             rel="noopener noreferrer"
             className="text-blue-600 hover:underline ml-1"
           >
-            Him
+            this guy
           </a>
         </span>
       </div>
