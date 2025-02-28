c4dd953b7c1e25e96d9da42201f8b9a05c6a1600
manishraj27 <manish012raj@gmail.com>
2025-02-28 23:29:51 +0530

    feat: refactor FooterContent to ensure FooterCredits is rendered correctly

--- src/components/ui/FooterContent.jsx
+++ src/components/ui/FooterContent.jsx
@@ -1,23 +1,14 @@
-
-
 import { Copyright } from "lucide-react";
 import FooterBottom from "./FooterBottom";
 import FooterCredits from "./FooterCredits";
 
 function FooterContent() {
-
-
   return (
     <div className="h-full w-full flex flex-col justify-end pb-4 px-4 lg:px-12">
-
-
-
-
       <div className="w-full flex flex-col lg:flex-row items-center justify-between lg:gap-24">
         <FooterBottom />
       </div>
-     <FooterCredits />
-
+      <FooterCredits />
     </div>
   );
 }
