41384c263f1b1d43f01843c52e96bb64924fc63c
manishraj27 <manish012raj@gmail.com>
2025-03-13 00:50:41 +0530

    fix: correct import name for ServicesSection and update Service component structure; add additional content paragraphs

--- src/components/pages/Service.jsx
+++ src/components/pages/Service.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import SerivesSection from '../ui/ServicesSection'
+import React from 'react';
+import ServicesSection from '../ui/ServicesSection';
 
 function Service() {
   return (
     <section
-    id="service"
-    aria-label="service"
-    className=" rounded-b-3xl overflow-hidden lg:px-12 px-4 w-full lg:py-16 py-64 min-h-screen dark-section bg-background-100 flex flex-col items-center justify-center"
-  >
-<SerivesSection />
+      id="service"
+      aria-label="service"
+      className="rounded-b-3xl overflow-visible lg:px-12 px-4 w-full lg:py-16 py-64 min-h-screen dark-section bg-background-100 flex flex-col items-center justify-center"
+    >
+      <ServicesSection />
+     
+      <p className="lg:text-heading-3 text-heading-4 text-on-dark">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque laborum, ipsa maxime sequi ratione sunt unde facere a dolores est, quod maiores recusandae saepe dolorem impedit. Veniam velit explicabo officiis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, iure aperiam qui ea hic, repudiandae vitae neque praesentium velit asperiores reiciendis cumque unde illum quae eos perferendis distinctio repellat quas?Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla saepe laboriosam nisi cupiditate! Fuga non eaque tempore fugiat. Magni dolore praesentium veniam, nesciunt rem id ratione quidem temporibus animi pariatur!
+      </p>
 
-<p className="lg:text-heading-3 text-heading-4  text-on-dark">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque laborum, ipsa maxime sequi ratione sunt unde facere a dolores est, quod maiores recusandae saepe dolorem impedit. Veniam velit explicabo officiis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, iure aperiam qui ea hic, repudiandae vitae neque praesentium velit asperiores reiciendis cumque unde illum quae eos perferendis distinctio repellat quas?Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla saepe laboriosam nisi cupiditate! Fuga non eaque tempore fugiat. Magni dolore praesentium veniam, nesciunt rem id ratione quidem temporibus animi pariatur!
-    </p>
+
+      
+      <p className="lg:text-heading-3 text-heading-4 text-on-dark">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque laborum, ipsa maxime sequi ratione sunt unde facere a dolores est, quod maiores recusandae saepe dolorem impedit. Veniam velit explicabo officiis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, iure aperiam qui ea hic, repudiandae vitae neque praesentium velit asperiores reiciendis cumque unde illum quae eos perferendis distinctio repellat quas?Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla saepe laboriosam nisi cupiditate! Fuga non eaque tempore fugiat. Magni dolore praesentium veniam, nesciunt rem id ratione quidem temporibus animi pariatur!
+      </p>
+
+
     </section>
-  )
+  );
 }
 
-export default Service
+export default Service;
